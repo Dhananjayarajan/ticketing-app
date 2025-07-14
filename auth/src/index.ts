f578c8cbb07248b4869ac008f377af9c21b72b17
Dhananjayarajan <162829820+Dhananjayarajan@users.noreply.github.com>
2025-07-15 05:16:11 +0530

    Auth routes added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express'
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signOutRouter } from './routes/signout'
+import { signUpRouter } from './routes/signup'
 
 const app = express()
 app.use(express.json())
 
-app.get('/api/users/currentuser', (req, res)=> {
-	res.send("Hi there")
-})
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
+
 
 app.listen(3000, ()=>{
 	console.log("Port is 3000!!!!")
